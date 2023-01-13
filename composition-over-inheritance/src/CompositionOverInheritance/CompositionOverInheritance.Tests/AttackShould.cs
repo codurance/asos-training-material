ca6b750cab4a841aa4fe52856a7f61f347564706
yeray <yeray.cabello@codurance.com>
2023-01-13 11:38:57 +0100

    red

--- composition-over-inheritance/src/CompositionOverInheritance/CompositionOverInheritance.Tests/AttackShould.cs
+++ composition-over-inheritance/src/CompositionOverInheritance/CompositionOverInheritance.Tests/AttackShould.cs
@@ -5,21 +5,25 @@ public class AttackShould
     [Fact(DisplayName = "do 30 damage for a biting monster")]
     public void Test1()
     {
-        Monster biter = new BitingMonster();
+        IAttack bite = new Bite();
+        Monster biter = new Monster(bite);
         Assert.Equal(30, biter.Attack());
     }
 
     [Fact(DisplayName = "do 45 damage for a kicking monster")]
     public void Test2()
     {
-        Monster biter = new KickingMonster();
-        Assert.Equal(45, biter.Attack());
+        IAttack kick = new Kick();
+        Monster kicker = new Monster(kick);
+        Assert.Equal(45, kicker.Attack());
     }
 
     [Fact(DisplayName = "do 75 damage for a monster that bites and kicks")]
     public void Test3()
     {
-        Monster kickerAndBiter = new KickingAndBitingMonster();
+        IAttack bite = new Bite();
+        IAttack kick = new Kick();
+        Monster kickerAndBiter = new Monster(bite, kick);
         Assert.Equal(75, kickerAndBiter.Attack());
     }
 }
